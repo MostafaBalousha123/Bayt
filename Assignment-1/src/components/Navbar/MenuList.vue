fa2f444ca2ff180ec1014c5f3220c500cdbbde3c
MostafaBalousha123 <mbalousha2001@gmail.com>
2023-06-16 21:32:35 +0300

    setup vue app and create navbar

---
+++ Assignment-1/src/components/Navbar/MenuList.vue
@@ -0,0 +1,44 @@
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const menuItems = ref([
+      { id: 1, label: 'Home', url: '#' },
+      { id: 2, label: 'About', url: '#' },
+      { id: 3, label: 'Services', url: '#' },
+      { id: 4, label: 'Contact', url: '#' }
+    ])
+    
+    return {
+      menuItems
+    }
+  }
+}
+</script>
+
+<template>
+  <ul class="links-menu">
+    <li v-for="item in menuItems" :key="item.id">
+      <a :href="item.url">{{ item.label }}</a>
+    </li>
+  </ul>
+</template>
+
+
+
+<style scoped>
+  ul {
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 15px;
+    padding: 15px;
+  }
+  ul a {
+    text-decoration: none;
+    color: white;
+  }
+</style>
---
+++ Assignment-1/src/components/Navbar/Navbar.vue
@@ -0,0 +1,55 @@
+<script>
+import MenuList from './MenuList.vue';
+import MobileDropdown from './MobileDropdown.vue'
+
+export default {
+  components: {
+    MobileDropdown,
+    MenuList
+}
+}
+</script>
+
+<template>
+  <div>
+    <nav>
+      <div class="logo">Logo</div>
+      <MobileDropdown>
+        <MenuList />
+      </MobileDropdown>
+      <MenuList class="links" />
+    </nav>
+    <!-- Your other content here -->
+  </div>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  background-color: #0d1721;
+  color: white;
+  height: 45px;
+}
+
+.logo {
+  font-weight: bold;
+}
+
+.links {
+  display: none;
+}
+
+@media (min-width: 768px) {
+  nav {
+  justify-content: start;
+}
+  .links {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+  }
+}
+</style>
